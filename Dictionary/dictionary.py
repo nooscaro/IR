731c1dde5058633766dc01d92a8969e23e45ff57
nooscaro <vira.baklanova@gmail.com>
2018-01-21 20:57:41 +0200

    the one that does work, although I am not sure what exactly it does

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -1,27 +1,28 @@
-import tokenize
 import glob
 
 
 class Dictionary:
-    dict = set()
-    num_words = 0
-    words_in_collection = 0
-    collection_size = 0
+
 
     def __init__(self, file_list):
+        self.dict = set()
+        self.num_words = 0
+        self.words_in_collection = 0
+        self.collection_size = 0
         for f in file_list:
-            list_of_words = tokenize.tokenize(open(f,"r").read())
-            for word in list_of_words:
+            list_of_words = open(f, "r").read()
+            tokenized_list_of_words = list_of_words.split()
+            for word in tokenized_list_of_words:
                 self.collection_size += 1
                 self.words_in_collection += 1
-                if not word in self.dict:
+                if word not in self.dict:
                     self.dict.add(word)
                     self.num_words += 1
-        dict_file = open("dict","w+")
-        for word in dict:
-            dict_file.write(word)
-        dict_file.write("Collection size = "+self.collection_size+"\nTotal words in collection = "+self.words_in_collection+"\nWords in dictionary = "+self.num_words);
-        print("Collection size = " + self.collection_size + "\nTotal words in collection = " + self.words_in_collection + "\nWords in dictionary = " + self.num_words);
+        dict_file = open("dict.txt", "w+")
+        for word in self.dict:
+            dict_file.write(word+'\n')
+        # dict_file.write("Collection size = "+self.collection_size+"\nTotal words in collection = "+self.words_in_collection+"\nWords in dictionary = "+self.num_words);
+        # print("Collection size = " + self.collection_size + "\nTotal words in collection = " + self.words_in_collection + "\nWords in dictionary = " + self.num_words);
 
 
 if __name__ == '__main__':
