b170666e7b8cdb96d2df010b26ec28db0a945446
nooscaro <vira.baklanova@gmail.com>
2018-01-21 22:12:45 +0200

    the one that kinda works

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -1,5 +1,5 @@
 import glob
-
+import re
 
 class Dictionary:
     def __init__(self, file_list):
@@ -9,13 +9,13 @@ class Dictionary:
         self.collection_size = 0
         for f in file_list:
             list_of_words = open(f, "r").read()
-            tokenized_list_of_words = list_of_words.split()
+            tokenized_list_of_words = re.split("[,\s\n\-\u2014!?:.;()\"]+", list_of_words)
             for word in tokenized_list_of_words:
-                self.collection_size += 1
                 self.words_in_collection += 1
-                if word not in self.dict:
-                    self.dict.add(word)
+                if word.upper() not in self.dict:
+                    self.dict.add(word.upper())
                     self.num_words += 1
+            self.collection_size+=1
         dict_file = open("dict.txt", "w+")
         dict_file.write("Files processed: "+str(self.collection_size)+'\n')
         dict_file.write("Total words: "+str(self.words_in_collection)+'\n')
