a7f364aa08fc975464c51ae129f885f8e88e3809
nooscaro <vira.baklanova@gmail.com>
2018-01-28 16:24:02 +0200

    incidence matrix

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -4,19 +4,23 @@ import re
 
 class Dictionary:
     def __init__(self, file_list):
-        self.dict = set()
+        self.dict = []
         self.num_words = 0
         self.words_in_collection = 0
-        self.collection_size = 0
+        self.collection_size = len(file_list)
+        self.matrix = {}
+        count = 0
         for f in file_list:
             list_of_words = open(f, "r").read()
             tokenized_list_of_words = re.split("[,\s\n\-\u2014!?:.;()\"]+", list_of_words)
             for word in tokenized_list_of_words:
                 self.words_in_collection += 1
                 if word.upper() not in self.dict:
-                    self.dict.add(word.upper())
+                    self.dict.append(word.upper())
                     self.num_words += 1
-            self.collection_size += 1
+                    self.init_list(word)
+                self.matrix.get(word.upper())[count] = 1
+            count += 1
         dict_file = open("dict.txt", "w+")
         dict_file.write("Files processed: "+str(self.collection_size)+'\n')
         dict_file.write("Total words: "+str(self.words_in_collection)+'\n')
@@ -24,9 +28,22 @@ class Dictionary:
         for word in self.dict:
             dict_file.write(word+'\n')
         dict_file.close()
+        matrix_file = open("matrix.txt","w+")
+        for token in self.matrix.iterkeys():
+            matrix_file.write(token+'\t')
+            for i in self.matrix.get(token):
+                matrix_file.write(str(i)+'\t')
+            matrix_file.write('\n')
+        matrix_file.close()
         print("Files processed: " + str(self.collection_size) + '\n')
         print("Total words: " + str(self.words_in_collection) + '\n')
         print("Total unique words: " + str(self.num_words))
+
+    def init_list(self, word):
+        list_temp = []
+        for i in range(0,self.collection_size,1):
+            list_temp.append(0)
+        self.matrix[word.upper()] = list_temp
 
 
 if __name__ == '__main__':
