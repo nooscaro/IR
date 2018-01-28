ddf3612c2fbf584e5f10b808c84c69a5d4b646aa
nooscaro <vira.baklanova@gmail.com>
2018-01-28 23:24:48 +0200

    test data added and bug fixing

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -2,6 +2,9 @@ import glob
 import re
 
 
+
+
+
 class Dictionary:
     def __init__(self, file_list):
         self.dict = []
@@ -10,8 +13,10 @@ class Dictionary:
         self.collection_size = len(file_list)
         self.matrix = {}
         self.inverted_index = {}
+        self.files = []
         count = 0
         for f in file_list:
+            self.files.append(f)
             list_of_words = open(f, "r").read()
             tokenized_list_of_words = re.split("[,\s\n\-\u2014!?:.;()\"]+", list_of_words)
             for word in tokenized_list_of_words:
@@ -25,21 +30,21 @@ class Dictionary:
                 if count not in self.inverted_index[word.upper()]:
                     self.inverted_index.get(word.upper()).append(count)
             count += 1
-        dict_file = open("dict.txt", "w+")
+        dict_file = open("Output/dict.txt", "w+")
         dict_file.write("Files processed: "+str(self.collection_size)+'\n')
         dict_file.write("Total words: "+str(self.words_in_collection)+'\n')
         dict_file.write("Total unique words: "+str(self.num_words)+'\n'+"--------------------------------"+'\n')
         for word in self.dict:
             dict_file.write(word+'\n')
         dict_file.close()
-        matrix_file = open("matrix.txt","w+")
+        matrix_file = open("Output/matrix.txt","w+")
         for token in self.matrix.iterkeys():
             matrix_file.write(token+'\t')
             for i in self.matrix.get(token):
                 matrix_file.write(str(i)+'\t')
             matrix_file.write('\n')
         matrix_file.close()
-        inverted_index_file = open("index.txt", "w+")
+        inverted_index_file = open("Output/index.txt", "w+")
         for token in self.inverted_index.iterkeys():
             inverted_index_file.write(token+'\t')
             for i in self.inverted_index.get(token):
@@ -56,9 +61,64 @@ class Dictionary:
             list_temp.append(0)
         self.matrix[word.upper()] = list_temp
 
+    def boolean_or(self, list_one, list_two):
+        res = []
+        for i in range(0,self.collection_size,1):
+            if i in list_one or i in list_two:
+                res.append(i)
+        return res
+
+    def boolean_and(self, list_one, list_two):
+        res = []
+        for i in range(0,self.collection_size,1):
+            if i in list_one and i in list_two:
+                res.append(i)
+        return res
+
+    def files_by_indices (self,l):
+        new_list = []
+        for i in l:
+            new_list.append(self.files[i])
+        return new_list
+
+    def parse_request(self,request):
+        tokens = request.split()# re.split(" ",request)
+        current = []
+        if not self.word(tokens[0].upper()):
+            return False
+        for i in self.inverted_index[tokens[0].upper()]:
+            current.append(i)
+
+        for i in range(1, len(tokens)-1, 2):
+            if tokens[i] == "&&":
+                if self.word(tokens[i+1].upper()):
+                    current = self.boolean_and(current,self.inverted_index[tokens[i+1].upper()])
+                else:
+                    return False
+            elif tokens[i] == "||":
+                if self.word(tokens[i+1].upper()):
+                    current = self.boolean_or(current, self.inverted_index[tokens[i+1].upper()])
+            else:
+                return False
+        self.print_result(req, self.files_by_indices(current))
+
+    def print_result(self, string, list):
+        print("Search results for: "+string+'\n\n')
+        for l in list:
+            print(l+'\n')
+
+
+    def word(self,str):
+        return str!="&&" and str!="||" and str in self.dict
+
 
 if __name__ == '__main__':
     collection = []
-    for filename in glob.glob('*.txt'):
+    for filename in glob.glob("Data/Samples/*.txt"):
+        print(filename)
         collection.append(filename)
     dictionary = Dictionary(collection)
+    # word_one = raw_input("Search for: ")
+    # word_two = raw_input("...AND: ")
+    req = raw_input("Search request using '&&' (and) and '||' (or): ")
+    dictionary.parse_request(req)
